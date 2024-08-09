9223b53020c87004e214aeef0be79dfecc63ad2d
ahmedafnouch <ahmedafnouch17@gmail.com>
2024-08-09 14:17:21 +0100

    en progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Search from './components/Search'
 import FoodList from './components/FoodList'
 import Nav from './components/Nav'
 import "./App.css"
+import Container from './components/Container'
+import InnerContainer from './components/InnerContainer'
 
 
 function App() {
@@ -13,7 +15,11 @@ function App() {
     <div className="App">
       <Nav /> 
       <Search  foodData = {foodData} setFoodData = {setFoodData}/> 
+      <Container>
+      <InnerContainer > 
       <FoodList foodData = {foodData} />
+      </InnerContainer>
+      </Container>
     </div>
   )
 }
--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,12 +1,15 @@
+import styles from './Fooditem.module.css'
+
 export default function FoodItem({ food }) {
     return (
-      <div>
-    
-    <img src={food.image} />
-      <h1>{food.title}</h1>
-
-      <button>View Recipe</button>
-    
+      <div className={styles.utemContainer}>
+    <img className={styles.itemimage} src={food.image} />
+      <div className={styles.itemContent}>
+      <p className={styles.itemName}>{food.title}</p>
+      </div>
+      <div className={styles.buttonContainer}>
+      <button className={styles.buttomitem}>View Recipe</button>
+      </div>
       </div>
     );
   }
